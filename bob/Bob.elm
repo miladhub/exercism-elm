7b87e763ed3dfc23c5cc5294e7054a7096369d64
Milad Bourhani <milad.bourhani@gmail.com>
2018-12-11 21:47:58 +0100

    improved Bob

--- bob/Bob.elm
+++ bob/Bob.elm
@@ -2,24 +2,24 @@ module Bob exposing (hey)
 
 hey : String -> String
 hey s =
-    if yelledquestion s then "Calm down, I know what I'm doing!"
+    if yelledQuestion s then "Calm down, I know what I'm doing!"
     else if question s then "Sure."
-    else if nosentence s then "Fine. Be that way!"
+    else if noSentence s then "Fine. Be that way!"
     else if yell s then "Whoa, chill out!"
     else "Whatever."
 
 question : String -> Bool
 question s =
-    (not << List.isEmpty << String.words) s && (String.right 1 s == "?")
+    (not << List.isEmpty << String.words) s && (String.endsWith "?") s
 
 yell : String -> Bool
 yell s =
     (String.any Char.isUpper) s && (not << String.any Char.isLower) s
 
-yelledquestion : String -> Bool
-yelledquestion s =
-    (question s) && (yell s)
+yelledQuestion : String -> Bool
+yelledQuestion s =
+    question s && yell s
 
-nosentence : String -> Bool
-nosentence =
+noSentence : String -> Bool
+noSentence =
     List.isEmpty << List.filter (String.any Char.isAlphaNum) << String.words
